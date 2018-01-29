9de1bef93e57e389e493082f0486c412658fa213
Jed <jed@JedsadaSaengow.local>
2018-01-29 18:25:45 +0700

    First commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TrainingDotNetCoreMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int id)
+        {
+            return $"Hello, Your name's {name}. Your ID is {id}";
+        }
+    }
+}
